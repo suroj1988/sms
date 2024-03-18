128adba53f3a969ad2a231b1fe1e30746b0d4aa0
Suroj Maharjan <surojmaharjan70@gmail.com>
2024-03-18 09:10:27 +0545

    Next change

--- home/models.py
+++ home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    Type = ((1,'Hod'),(2,'Student'),(3,'Teacher'))
+
+    user_type = models.CharField(choices=Type,max_length=200,default=1)
+    profile_pic = models.ImageField(upload_to='media')
+
+
