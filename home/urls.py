022aa4ad2bb6a8dda04b961cff5a0f0fcdfd24b9
Suroj Maharjan <surojmaharjan70@gmail.com>
2024-03-21 19:12:39 +0545

    Next change

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,8 @@ from .views import *
 from django.urls import path
 
 urlpatterns = [
-    path('', home, name='home')
+    path('', home, name='home'),
+    path('login', dologin, name='dologin'),
+
 
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,31 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,HttpResponse
+from django.contrib.auth import authenticate,login,logout
+from home.EmailBackEnd import EmailBackEnd
+from django.contrib import messages
+
 
 # Create your views here.
 def home(request):
     return render(request,'login.html')
+
+def dologin(request):
+    if request.method == "POST":
+        user = EmailBackEnd.authenticate(request,username=request.POST.get('email'),password=request.POST.get('password'))
+        if user != None:
+            login(request, user)
+            user_type = user.user_type
+            if user_type == '1':
+                return HttpResponse("HOd Login")
+            elif user_type == '2':
+                return HttpResponse("student")
+            elif user_type == '3':
+                return HttpResponse("teacher")
+            else:
+              messages.error(request,"username and password invalid")
+              return redirect("/")
+        else:
+            messages.error(request, "username and password invalid")
+            return redirect("/")
+
+
 
